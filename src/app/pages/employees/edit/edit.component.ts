454c30f50e326afbed7c1ca2cbda7361f2112e80
Clerison <cosclerison@gmail.com>
2022-10-30 19:31:10 -0300

    Ação botão edit da tela List para tela de Edit

--- src/app/pages/employees/edit/edit.component.ts
+++ src/app/pages/employees/edit/edit.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,8 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit.component.scss']
 })
 export class EditComponent implements OnInit {
+  
+  /**
+   * Criando uma propriedade com valor vazio para ser inserido
+   *  o valor que vem do getCurrentNavigation
+   */
+  value: any;
 
-  constructor() { }
+  constructor(
+    private touter: Router
+  ) { 
+    /**
+     * Criado uma constante para receber o valor vindo da navegação da rota que foi
+     * utilizada em outra tela.
+     * direcionado o valor para a propriedade com valor vazio,
+     * assim atribuído o valor e ela.
+     * deixamos os itens como opcionais caso tenha valor nos mesmos
+     */
+    const navigation = this.touter.getCurrentNavigation();
+    this.value = navigation?.extras.state;
+  }
 
   ngOnInit(): void {
   }
--- src/app/pages/employees/list/list.component.ts
+++ src/app/pages/employees/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Router } from '@angular/router';
+import { NavigationExtras, Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.component.scss']
 })
 export class ListComponent implements OnInit {
+  /**
+   * Criado propriedade para ser usado o seu estado de objeto,
+   * assim atribuindo valor a ele quando receber dados da ação do (click)
+   * Objeto com estado de vazio para não ocorrer erro ou concatenar os valores
+   */
+  navigationExtras: NavigationExtras = {
+    state: {
+      value: null
+    }
+  };
 
   constructor(
     private router: Router
@@ -15,13 +25,26 @@ export class ListComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  onGoToEdit(Item: any): void{
-    this.router.navigate(['edit']);
+  onGoToEdit(item: any): void{
+    /**
+     * adicionado o valor de item no estado do objeto navigationExtras,
+     * atribuído o valor como segundo parâmetro na rota de navegação (assim a rota de navegação envia o valor
+     * que será recebido pela tela corrente, fazendo com que o valor recebido seja utilizado)
+     */
+    this.navigationExtras.state = item;
+    this.router.navigate(['edit'], this.navigationExtras);
   }
-  onGoToDetails(Item: any): void{
-    this.router.navigate(['details']);
+
+  onGoToDetails(item: any): void{
+    /**
+     * adicionado o valor de item no estado do objeto navigationExtras,
+     * atribuído o valor como segundo parâmetro na rota de navegação (assim a rota de navegação envia o valor
+     * que será recebido pela tela corrente, fazendo com que o valor recebido seja utilizado)
+     */
+    this.navigationExtras.state = item;
+    this.router.navigate(['details'], this.navigationExtras);
   }
-  onGoToDelete(Item: any): void{
+  onGoToDelete(item: any): void{
     alert('Deleted');
   }
 
